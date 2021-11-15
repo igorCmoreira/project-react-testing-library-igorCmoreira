7d1075b40e9c3fdc3f8ba4a26ffb2e7933a6f0b2
Igor Campos Moreira <igorcampos200@gmail.com>
2021-11-14 21:24:49 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
+
+describe('#Requisito 2-> Teste o componente <About.js />.', () => {
+  it('Verifica se a um "h2" com o texto "About Pokédex."', () => {
+    renderWithRouter(<About />);
+
+    const pageTitle = screen.getByRole('heading', { name: 'About Pokédex' });
+
+    expect(pageTitle).toBeDefined();
+  });
+  it('Verifica se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    renderWithRouter(<About />);
+
+    const paragraph1 = screen.getByText(/This application/i);
+    const paragraph2 = screen.getByText(/One can filter/i);
+
+    expect(paragraph1).toBeInTheDocument();
+    expect(paragraph2).toBeInTheDocument();
+  });
+  it('Verifica se a página contém a imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const { src } = screen.getByRole('img');
+
+    expect(src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
